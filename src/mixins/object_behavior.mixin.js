947e114c6ad63e95a3f137dcbc0167e047203010
Dennis John <dennis.r.john@gmail.com>
2018-06-26 16:04:00 -0600

    simplified the logic

--- src/mixins/object_behavior.mixin.js
+++ src/mixins/object_behavior.mixin.js
@@ -3,24 +3,37 @@
   /**
    * Override _setObjectScale and apply max and min dimensions
    */
+  var setObjectScaleOverridden = fabric.Canvas.prototype._setObjectScale;
 
   fabric.Canvas.prototype._setObjectScale = function (localMouse, transform,
     lockScalingX, lockScalingY, by, lockScalingFlip, _dim) {
 
-    var returnValue = false;
     var t = transform.target;
-    var tw = t._getTransformedDimensions().x;
-    var w = t.width * (localMouse.x / tw);
-    if (w >= t.getMinWidth() && w <= t.getMaxWidth()) {
-      t.set('width', w);
-      returnValue = true;
+    if (by === 'x') {
+      var tw = t._getTransformedDimensions().x;
+      var w = t.width * (localMouse.x / tw);
+      if (w >= t.getMinWidth() && w <= t.getMaxWidth()) {
+        t.set('width', w);
+        return true;
+      }
     }
-    var th = t._getTransformedDimensions().y;
-    var h = t.height * (localMouse.y / th);
-    if (h >= t.getMinHeight() && h <= t.getMaxHeight()) {
-      t.set('height', h);
-      returnValue = true;
+    else if (by === 'y') {
+      var th = t._getTransformedDimensions().y;
+      var h = t.height * (localMouse.y / th);
+      if (h >= t.getMinHeight() && h <= t.getMaxHeight()) {
+        t.set('height', h);
+        return true;
+      }
     }
-    return returnValue;
+    else if (by === 'equally') {
+      var tw = t._getTransformedDimensions().x;
+      var w = t.width * (localMouse.x / tw);
+      var th = t._getTransformedDimensions().y;
+      var h = t.height * (localMouse.y / th);
+      if (w >= t.getMinWidth() && w <= t.getMaxWidth() && h >= t.getMinHeight() && h <= t.getMaxHeight()) {
+        return setObjectScaleOverridden.call(fabric.Canvas.prototype, localMouse, transform,
+          lockScalingX, lockScalingY, by, lockScalingFlip, _dim);
+      }
+    }
   };
 })();
