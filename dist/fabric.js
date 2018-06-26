9a4b81ab70ddddb034205963d8a6d8026498eb48
Dennis John <dennis.r.john@gmail.com>
2018-06-26 15:00:52 -0600

    added missing mixin

--- dist/fabric.js
+++ dist/fabric.js
@@ -14055,6 +14055,41 @@ fabric.util.object.extend(fabric.StaticCanvas.prototype, /** @lends fabric.Stati
 
 (function() {
 
+  /**
+   * Override _setObjectScale and apply max and min dimensions
+   */
+  var setObjectScaleOverridden = fabric.Canvas.prototype._setObjectScale;
+
+  fabric.Canvas.prototype._setObjectScale = function (localMouse, transform,
+    lockScalingX, lockScalingY, by, lockScalingFlip, _dim) {
+
+    var t = transform.target;
+    if (by === 'x') {
+      var tw = t._getTransformedDimensions().x;
+      var w = t.width * (localMouse.x / tw);
+      if (w >= t.getMinWidth() && w <= t.getMaxWidth()) {
+        t.set('width', w);
+        return true;
+      }
+    }
+    else if (by === 'y') {
+      var th = t._getTransformedDimensions().y;
+      var h = t.height * (localMouse.y / th);
+      if (h >= t.getMinHeight() && h <= t.getMaxHeight()) {
+        t.set('height', w);
+        return true;
+      }
+    }
+    else {
+      return setObjectScaleOverridden.call(fabric.Canvas.prototype, localMouse, transform,
+        lockScalingX, lockScalingY, by, lockScalingFlip, _dim);
+    }
+  };
+})();
+
+
+(function() {
+
   var degreesToRadians = fabric.util.degreesToRadians,
       originXOffset = {
         left: -0.5,
