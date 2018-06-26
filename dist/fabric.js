c3d321ff1da3aa404601eb5fe902f2a105e70557
Dennis John <dennis.r.john@gmail.com>
2018-06-26 15:36:02 -0600

    new build

--- dist/fabric.js
+++ dist/fabric.js
@@ -14058,32 +14058,25 @@ fabric.util.object.extend(fabric.StaticCanvas.prototype, /** @lends fabric.Stati
   /**
    * Override _setObjectScale and apply max and min dimensions
    */
-  var setObjectScaleOverridden = fabric.Canvas.prototype._setObjectScale;
 
   fabric.Canvas.prototype._setObjectScale = function (localMouse, transform,
     lockScalingX, lockScalingY, by, lockScalingFlip, _dim) {
 
+    var returnValue = false;
     var t = transform.target;
-    if (by === 'x') {
-      var tw = t._getTransformedDimensions().x;
-      var w = t.width * (localMouse.x / tw);
-      if (w >= t.getMinWidth() && w <= t.getMaxWidth()) {
-        t.set('width', w);
-        return true;
-      }
+    var tw = t._getTransformedDimensions().x;
+    var w = t.width * (localMouse.x / tw);
+    if (w >= t.getMinWidth() && w <= t.getMaxWidth()) {
+      t.set('width', w);
+      returnValue = true;
     }
-    else if (by === 'y') {
-      var th = t._getTransformedDimensions().y;
-      var h = t.height * (localMouse.y / th);
-      if (h >= t.getMinHeight() && h <= t.getMaxHeight()) {
-        t.set('height', w);
-        return true;
-      }
+    var th = t._getTransformedDimensions().y;
+    var h = t.height * (localMouse.y / th);
+    if (h >= t.getMinHeight() && h <= t.getMaxHeight()) {
+      t.set('height', h);
+      returnValue = true;
     }
-    else {
-      return setObjectScaleOverridden.call(fabric.Canvas.prototype, localMouse, transform,
-        lockScalingX, lockScalingY, by, lockScalingFlip, _dim);
-    }
+    return returnValue;
   };
 })();
 
