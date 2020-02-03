8d36b0daefbc42bc7596e64af1808e2aac3b3259
Dennis John <dennis.r.john@gmail.com>
2020-02-03 12:00:33 -0700

    build

--- dist/fabric.js
+++ dist/fabric.js
@@ -25598,7 +25598,7 @@ fabric.Image.filters.BaseFilter.fromObject = function(object, callback) {
       }
       // if there is text background color no
       // other shadows should be casted
-      this._removeShadow(ctx);
+      // this._removeShadow(ctx);
     },
 
     /**
